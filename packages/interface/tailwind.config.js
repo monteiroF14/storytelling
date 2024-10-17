3a81f0e722f0a98e3e9e33cc358010655fd1ffe0
monteiroF14 <119819402+monteiroF14@users.noreply.github.com>
2024-10-17 23:54:23 +0100

    style: add slide animations & font

--- packages/interface/tailwind.config.js
+++ packages/interface/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				"slide-in": "slideIn 0.3s ease-out",
+				"slide-out": "slideOut 0.3s ease-in",
+			},
+			keyframes: {
+				slideIn: {
+					"0%": { transform: "translateY(-20px)", opacity: "0" },
+					"100%": { transform: "translateY(0)", opacity: "1" },
+				},
+				slideOut: {
+					"0%": { transform: "translateY(0)", opacity: "1" },
+					"100%": { transform: "translateY(20px)", opacity: "0" },
+				},
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
