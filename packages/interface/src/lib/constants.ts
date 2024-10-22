0fd6e737f29966191cba4e753381a7235688ba86
monteiroF14 <119819402+monteiroF14@users.noreply.github.com>
2024-10-22 16:16:33 +0100

    refactor: update number of steps per storyline

--- packages/interface/src/lib/constants.ts
+++ packages/interface/src/lib/constants.ts
@@ -3,4 +3,4 @@ export const MAX_DESCRIPTION_LENGTH = 800;
 export const MAX_AI_RETRIES = 1;
 export const CHOICES = 3;
 
-export const TOTAL_STORYLINE_STEPS = 1;
+export const TOTAL_STORYLINE_STEPS = 8;
