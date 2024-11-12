9685fe1f4464e8832fb68b6bdf096d29627ef9a4
monteiroF14 <119819402+monteiroF14@users.noreply.github.com>
2024-11-12 19:49:17 +0000

    chore: update view storyline permissions file at client side

--- packages/interface/src/routes/storylines/[id]/+page.ts
+++ packages/interface/src/routes/storylines/[id]/+page.ts
@@ -1,20 +1,19 @@
+import { currentStoryline } from "$lib/stores";
 import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
-import { currentStoryline } from "$lib/stores";
 
 export const load: PageLoad = async ({ data, parent }) => {
 	const parentData = await parent();
 
 	const storyline = data.storyline;
 
-	if (!parentData.user) return;
+	const isLoggedIn = !!parentData.user;
+	const doesUserOwnStoryline =
+		isLoggedIn && storyline?.userId === parentData.user.id;
 
-	const hasPermission =
-		(storyline?.visibility === "private" &&
-			storyline.userId === parentData.user.id) ||
-		storyline?.visibility === "public";
+	const isStorylinePublic = storyline?.visibility === "public";
 
-	if (hasPermission) {
+	if (isStorylinePublic || (!isStorylinePublic && doesUserOwnStoryline)) {
 		currentStoryline.set(storyline);
 		return;
 	}
